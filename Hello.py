447978cf7c3ecbbe204624be584219017c3bc832
Evanessa Nkenfack <njifackv@gmail.com>
2024-04-17 14:16:32 -0400

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,10 @@
+# hello.py
+""" this code  asked users their age ,last and first name and return a welcome message"""
+LastName = input(str("what's your last name ? ")
+                 )  # ask user to  their last name
+GivenName = input(str("whats your given name ?"))  # ask user their given name
+age = input("whats your age ? ")
+              # ask user their age
+
+print(
+    f" Welcome to this new journey {GivenName} {LastName} \n your age {age} is a perfect match for this program")
